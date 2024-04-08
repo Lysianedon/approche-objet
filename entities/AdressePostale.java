3cd9c294b7115c645e9a789401d6911e16fef0c5
lysiandon <don.lysiane@gmail.com>
2024-04-08 16:48:43 +0200

    finished exercise

--- entities/AdressePostale.java
+++ entities/AdressePostale.java
@@ -1,21 +1,25 @@
 package entities;
 
 public class AdressePostale {
-
-	int numeroRue;
+	// Attributs d'instances => variables globales
+	int numeroRue; 
 	String libelleRue;
 	int codePostal;
 	String ville;
-	
+
+	//Constructeurs:
 	public AdressePostale() {
-		
+
 	}
+
 	public AdressePostale(int numeroRue, String libelleRue, int codePostal, String ville) {
-		this.numeroRue = numeroRue;
+		this.numeroRue = numeroRue; // on valorise les variables avec les paramètres du constructeur
 		this.libelleRue = libelleRue;
 		this.codePostal = codePostal;
 		this.ville = ville;
-		
+
 	}
+	
+	//Methodes ici:
 
 }
