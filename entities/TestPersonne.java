9b783fb724e28d95c2cd9d50a6c4fe69e2922c11
lysiandon <don.lysiane@gmail.com>
2024-04-08 16:49:09 +0200

    finished exercises

--- entities/TestPersonne.java
+++ entities/TestPersonne.java
@@ -1,15 +1,22 @@
 package entities;
+
 import entities2.Personne;
 
 public class TestPersonne {
 
 	public static void main(String[] args) {
-		
-		
-		AdressePostale adresse1 = new AdressePostale(67, "Rue de Flandre", 75020, "Paris" );		
+
+		AdressePostale adresse1 = new AdressePostale(67, "Rue de Flandre", 75020, "Paris");
 		AdressePostale adresse2 = new AdressePostale(627, "Rue Hachis", 75006, "Paris");
 
-		Personne pers1 = new Personne("Beatrice","Luengo", adresse1);		
+		Personne pers1 = new Personne("Beatrice", "Luengo", adresse1);
+		pers1.updateFirstName("Bea");
+		pers1.updateLastName("Lu");
+		pers1.updateAddress(adresse2);
+		System.out.println(pers1.displayLastName());
+		System.out.println(pers1.displayFirstName());
+		System.out.println(pers1.displayAddress());
+		
 		Personne pers2 = new Personne("Helene", "Segara", adresse2);
 
 	}
--- entities2/Personne.java
+++ entities2/Personne.java
@@ -1,20 +1,48 @@
 package entities2;
+
 import entities.AdressePostale;
 
 public class Personne {
-
+//Attributs d'instance:
 	public String nom;
 	public String prenom;
 	public AdressePostale adressePostale = new AdressePostale();
-	
+
 	public Personne(String nom, String prenom) {
 		this.nom = nom;
 		this.prenom = prenom;
 	}
 	
+//	Constructeurs:
+
 	public Personne(String nom, String prenom, AdressePostale adressePostale) {
-		this.nom = nom;
+		this.nom = nom; // On utilise "this" pour retirer les ambigüités de nommage
 		this.prenom = prenom;
 		this.adressePostale = adressePostale;
 	}
+	
+//	Methodes:
+	public void printName() {
+		System.out.println(nom.toUpperCase() + " " + prenom);
+	}
+	
+	public void updateFirstName(String prenom) {
+		this.prenom = prenom;
+	}
+	public void updateLastName(String nom) {
+		this.nom = nom;
+	}
+	public void updateAddress(AdressePostale adressePostale) {
+		this.adressePostale = adressePostale;
+	}
+	public String displayLastName() {
+		return nom;
+	}
+	public String displayFirstName() {
+		return prenom;
+	}
+	public AdressePostale displayAddress() {
+		return adressePostale;
+	}
 }
+
