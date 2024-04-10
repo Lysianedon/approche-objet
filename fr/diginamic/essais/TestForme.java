d83b2222b8dec539ea378f0b9bed8f158484ae72
lysiandon <don.lysiane@gmail.com>
2024-04-10 21:57:18 +0200

    finished exercise Forme

---
+++ fr/diginamic/essais/TestForme.java
@@ -0,0 +1,20 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.formes.Carre;
+import fr.diginamic.formes.Cercle;
+import fr.diginamic.formes.Rectangle;
+import fr.diginamic.essais.AffichageForme;
+
+public class TestForme {
+	
+	public static void main(String[] args) {
+		Cercle cercle = new Cercle(6);
+		Rectangle rectangle = new Rectangle(4, 6);
+		Carre carre = new Carre(6);
+		
+		AffichageForme.afficher(cercle);
+		AffichageForme.afficher(rectangle);
+		AffichageForme.afficher(carre);
+	}
+
+}
